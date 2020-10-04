81917a4f8073d1b4dced536fa95d4d3d86cd5ef4
Pasto <dpastierovic@gmail.com>
2020-10-04 22:08:24 +0200

    #7 Added paging

--- src/app/activity-list/activity-list.component.ts
+++ src/app/activity-list/activity-list.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { parse } from 'querystring';
 import { StravaApiService } from '../services/strava-api.service';
 
 @Component({
@@ -7,12 +8,47 @@ import { StravaApiService } from '../services/strava-api.service';
   styleUrls: ['./activity-list.component.css']
 })
 export class ActivityListComponent implements OnInit {
-
-  activities: number[] = [1, 2, 3];
+  @ViewChild('goToInput', {static: false}) goToInput;
+  activityCount;
+  activities;
+  pageCurrent
+  pageCount
+  pageSize = 10
+  goToNumber = 1
 
   constructor(private api: StravaApiService) { }
 
   ngOnInit() {
-    this.api.getStats().subscribe(_ => console.log(_))
+    this.api.getStats().subscribe(_ => {
+      this.activityCount = _['all_ride_totals']['count']
+      this.pageCount = Math.round(parseInt(this.activityCount) / this.pageSize)
+      this.pageCurrent = 1
+    })
+    this.api.getActivities(1, this.pageSize).subscribe(_ => this.activities = _)
+  }
+
+  onRenameClicked(item):void{
+    console.log(item)
+  }
+
+  onPageChanged(pageNumber: number){
+
+    if (pageNumber < 1) pageNumber = 1
+    if (pageNumber > this.pageCount) pageNumber = this.pageCount
+
+    console.log(pageNumber)
+    this.api.getActivities(pageNumber, this.pageSize).subscribe(_ => {
+      this.activities = _
+      this.pageCurrent = pageNumber
+    })
+  }
+
+  onKey(event){
+    console.log(event)
+    this.goToNumber = event.target.value
+  }
+
+  navigateToPage() {
+    this.onPageChanged(this.goToNumber)
   }
 }
